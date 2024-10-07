2af85edc1162f7486e6d3907c79a5c3f074ee4c2
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-10-07 04:53:10 +0300

    I'm blind

--- utils.py
+++ utils.py
@@ -314,7 +314,7 @@ def readRewards(network):
                     continue
                 date_string, amount = line.split("|")
                 amount = float(amount)
-                today = datetime.now().date()
+                today = datetime.now()
                 seven_days_ago = today - timedelta(days=7)
                 formatted_date = datetime.strptime(date_string, "%a, %d %b %Y %H:%M:%S %z") # First to object...
                 if seven_days_ago <= formatted_date <= today:
