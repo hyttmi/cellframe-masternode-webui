3fd6a992f0d9236f4798d363d754f819dbf4ed1e
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-10-07 05:02:19 +0300

    ...

--- utils.py
+++ utils.py
@@ -316,7 +316,7 @@ def readRewards(network):
                 amount = float(amount)
                 today = datetime.now()
                 seven_days_ago = today - timedelta(days=7)
-                formatted_date = datetime.strptime(date_string, "%a, %d %b %Y %H:%M:%S %z") # First to object...
+                formatted_date = datetime.strptime(date_string, "%a, %d %b %Y %H:%M:%S") # First to object...
                 if seven_days_ago <= formatted_date <= today:
                     if formatted_date in rewards:
                         rewards[formatted_date.strftime("%a, %d %b %Y")] += amount
