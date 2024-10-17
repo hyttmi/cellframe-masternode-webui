d3a80a27ccef8439c829bab5b188700743de7f57
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-10-17 11:52:30 +0300

    Move threadpool launch to a separate thread on init()

--- cellframe-masternode-webui.py
+++ cellframe-masternode-webui.py
@@ -4,6 +4,7 @@ from handlers import *
 from utils import *
 from mailer import sendMail
 from telegram import sendTelegram
+import threading
 
 def HTTPServer():
     try:
@@ -15,6 +16,11 @@ def HTTPServer():
     return 0
 
 def init():
+    t = threading.Thread(target=startInitialization)
+    t.start()
+    return 0
+
+def startInitialization():
     email_stats_enabled = getConfigValue("webui", "email_stats", default=False)
     email_stats_time = getConfigValue("webui", "email_time")
     telegram_stats_enabled = getConfigValue("webui", "telegram_stats", default=False)
@@ -24,20 +30,22 @@ def init():
             
     with ThreadPoolExecutor() as executor:
         executor.submit(HTTPServer)
+        
         if email_stats_enabled and validateTime(email_stats_time):
             logNotice(f"Email sending is activated at {email_stats_time}")
             executor.submit(sendMail, f"Email sending is activated at {email_stats_time}")
             executor.submit(funcScheduler, lambda: sendMail(generateHTML("mail.html")), email_stats_time)
+        
         if telegram_stats_enabled and validateTime(telegram_stats_time):
-            executor.submit(sendTelegram, f"Telegram sending is activated at {telegram_stats_time}")
             logNotice(f"Telegram sending is activated at {telegram_stats_time}")
+            executor.submit(sendTelegram, f"Telegram sending is activated at {telegram_stats_time}")
             executor.submit(funcScheduler, lambda: sendTelegram(generateHTML("telegram.html")), telegram_stats_time)
+        
         if cache_rewards and validateNum(cache_rewards_time):
             if cache_rewards_time < 10:
                 logError("Rewards caching time is below 10 minutes which is not recommended as it uses lots of CPU. Consider higher value.")
             executor.submit(cacheRewards) # Run once on startup, then with the timer as we know that both settings are set
             executor.submit(funcScheduler, cacheRewards, False, cache_rewards_time)
-    return 0
 
 def deinit():
     logNotice("stopped")
