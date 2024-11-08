42d51b94fc6fd3dfcc559886472ec3babf9acdd7
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-11-08 19:03:53 +0200

    ...

--- logger.py
+++ logger.py
@@ -11,7 +11,7 @@ def getScriptDir():
 log_file = os.path.join(getScriptDir(), "webui.log")
 logging.basicConfig(
     filename=log_file,
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] [%(funcName)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
