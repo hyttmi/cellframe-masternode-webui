3630a1835c3940952cc9e053dfd8a20fd1a71e41
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-11-08 18:24:04 +0200

    ...

---
+++ logger.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import threading
+import inspect
+from utils import getScriptDir
+
+logLock = threading.Lock()
+
+log_file = os.path.join(getScriptDir(), "webui.log")
+logging.basicConfig(
+    filename=log_file,
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] [%(funcName)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+def logNotice(msg):
+    with logLock:
+        func_name = inspect.stack()[1].function
+        logging.info(f"[{func_name}] {msg}")
+
+def logError(msg):
+    with logLock:
+        func_name = inspect.stack()[1].function
+        logging.error(f"[{func_name}] {msg}")
+
+def logDebug(msg):
+    with logLock:
+        func_name = inspect.stack()[1].function
+        logging.debug(f"[{func_name}] {msg}")
