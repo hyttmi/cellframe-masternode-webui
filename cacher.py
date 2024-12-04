2b697319966317ec9da669a5833744acd9d455c0
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-12-04 03:26:42 +0200

    ...

--- cacher.py
+++ cacher.py
@@ -6,68 +6,66 @@ try:
     from logger import log_it
     from datetime import datetime
     from collections import OrderedDict
-    from config import Config
 except Exception as e:
     log_it(f"ImportError: {e}")
 
 def cache_blocks_data():
     try:
-        while True:
-            networks = get_active_networks()
-            for network in networks:
-                net_config = get_network_config(network)
-                if net_config:
-                    log_it("i", "Caching blocks...")
-                    start_time = time.time()
+        networks = get_active_networks()
+        for network in networks:
+            net_config = get_network_config(network)
+            if net_config:
+                log_it("i", "Caching blocks...")
+                start_time = time.time()
 
-                    block_data = {
-                        'block_count': 0,
-                        'signed_blocks_count': 0,
-                        'first_signed_blocks_count': 0,
-                        'all_signed_blocks': {}
+                block_data = {
+                    'block_count': 0,
+                    'signed_blocks_count': 0,
+                    'first_signed_blocks_count': 0,
+                    'all_signed_blocks': {}
+                }
+
+                with ProcessPoolExecutor() as pexecutor:
+                    futures = {
+                        'block_count': pexecutor.submit(cli_command, f"block count -net {network}"),
+                        'first_signed_blocks': pexecutor.submit(cli_command, f"block list -net {network} first_signed -cert {net_config['blocks_sign_cert']} -limit 1"),
+                        'signed_blocks': pexecutor.submit(cli_command, f"block list -net {network} signed -cert {net_config['blocks_sign_cert']}")
                     }
 
-                    with ProcessPoolExecutor() as pexecutor:
-                        futures = {
-                            'block_count': pexecutor.submit(cli_command, f"block count -net {network}"),
-                            'first_signed_blocks': pexecutor.submit(cli_command, f"block list -net {network} first_signed -cert {net_config['blocks_sign_cert']} -limit 1"),
-                            'signed_blocks': pexecutor.submit(cli_command, f"block list -net {network} signed -cert {net_config['blocks_sign_cert']}")
-                        }
+                block_count_result = futures['block_count'].result()
+                block_count_match = re.search(r":\s+(\d+)", block_count_result)
+                if block_count_match:
+                    block_data['block_count'] = int(block_count_match.group(1))
 
-                    block_count_result = futures['block_count'].result()
-                    block_count_match = re.search(r":\s+(\d+)", block_count_result)
-                    if block_count_match:
-                        block_data['block_count'] = int(block_count_match.group(1))
+                signed_blocks_result = futures["signed_blocks"].result()
+                signed_blocks_match = re.search(r"have blocks: (\d+)", signed_blocks_result)
+                if signed_blocks_match:
+                    block_data['signed_blocks_count'] = int(signed_blocks_match.group(1))
 
-                    signed_blocks_result = futures["signed_blocks"].result()
-                    signed_blocks_match = re.search(r"have blocks: (\d+)", signed_blocks_result)
-                    if signed_blocks_match:
-                        block_data['signed_blocks_count'] = int(signed_blocks_match.group(1))
+                first_signed_match = re.search(r"have blocks: (\d+)", futures["first_signed_blocks"].result())
+                if first_signed_match:
+                    block_data['first_signed_blocks_count'] = int(first_signed_match.group(1))
 
-                    first_signed_match = re.search(r"have blocks: (\d+)", futures["first_signed_blocks"].result())
-                    if first_signed_match:
-                        block_data['first_signed_blocks_count'] = int(first_signed_match.group(1))
+                blocks_signed_per_day = {}
+                lines = signed_blocks_result.splitlines()
+                for line in lines:
+                    if "ts_create:" in line:
+                        timestamp_str = line.split("ts_create:")[1].strip()[:-6]
+                        block_time = datetime.strptime(timestamp_str, "%a, %d %b %Y %H:%M:%S")
+                        block_day = block_time.strftime("%a, %d %b %Y")
+                        blocks_signed_per_day[block_day] = blocks_signed_per_day.get(block_day, 0) + 1
 
-                    blocks_signed_per_day = {}
-                    lines = signed_blocks_result.splitlines()
-                    for line in lines:
-                        if "ts_create:" in line:
-                            timestamp_str = line.split("ts_create:")[1].strip()[:-6]
-                            block_time = datetime.strptime(timestamp_str, "%a, %d %b %Y %H:%M:%S")
-                            block_day = block_time.strftime("%a, %d %b %Y")
-                            blocks_signed_per_day[block_day] = blocks_signed_per_day.get(block_day, 0) + 1
+                sorted_blocks = OrderedDict(sorted(blocks_signed_per_day.items(), key=lambda x: datetime.strptime(x[0], "%a, %d %b %Y")))
+                block_data["all_signed_blocks"] = sorted_blocks
 
-                    sorted_blocks = OrderedDict(sorted(blocks_signed_per_day.items(), key=lambda x: datetime.strptime(x[0], "%a, %d %b %Y")))
-                    block_data["all_signed_blocks"] = sorted_blocks
+                cache_file_path = os.path.join(get_current_script_directory(), f".{network}_blocks_cache.json")
+                with open(cache_file_path, "w") as f:
+                    json.dump(block_data, f, indent=4)
 
-                    cache_file_path = os.path.join(get_current_script_directory(), f".{network}_blocks_cache.json")
-                    with open(cache_file_path, "w") as f:
-                        json.dump(block_data, f, indent=4)
-
-                    elapsed_time = time.time() - start_time
-                    log_it("i",f"Blocks cached for {network}! It took {elapsed_time:.2f} seconds!")
-                log_it("e", f"Network config not found for {network}, skipping caching")
-                return None
+                elapsed_time = time.time() - start_time
+                log_it("i",f"Blocks cached for {network}! It took {elapsed_time:.2f} seconds!")
+            log_it("e", f"Network config not found for {network}, skipping caching")
+            return None
     except Exception as e:
         log_it("e", f"Error: {e}")
         
