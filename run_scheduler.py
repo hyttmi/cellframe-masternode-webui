f5da8a10116d191058ef07b71c2a3d3550b5ea57
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-12-05 05:09:27 +0200

    ...

--- run_scheduler.py
+++ run_scheduler.py
@@ -6,8 +6,8 @@ def run_scheduler(func, scheduled_time, every_min=False):
     try:
         scheduler = schedule.Scheduler()
         if every_min:
-            scheduler.every(every_min).minutes.do(func)
-            log_it("i", f"Scheduling {func.__name__} to run every {every_min} minutes.")
+            scheduler.every(scheduled_time).minutes.do(func)
+            log_it("i", f"Scheduling {func.__name__} to run every {scheduled_time} minutes.")
         else:
             scheduler.every().day.at(scheduled_time).do(func)
             log_it("i", f"Scheduling {func.__name__} to run daily at {scheduled_time}.")
