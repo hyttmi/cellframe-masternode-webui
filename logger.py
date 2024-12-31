3c9181a8ae59585fa9069e1dac9cdcf3baee36f2
Mika Hyttinen <mika.hyttinen@gmail.com>
2024-12-31 12:23:19 +0200

    logging is thread safe so remove loglock, add log rotation

--- logger.py
+++ logger.py
@@ -1,39 +1,45 @@
 import logging
 import os
-import threading
 import inspect
 from config import Config
-
-logLock = threading.Lock()
+from logging.handlers import RotatingFileHandler
 
 log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "webui.log")
-logging.basicConfig(
-    filename=log_file,
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] [%(funcName)s] %(message)s",
+
+handler = RotatingFileHandler(
+    log_file,
+    maxBytes=5 * 1024 * 1024,
+    backupCount=5
+)
+
+formatter = logging.Formatter(
+    "%(asctime)s [%(levelname)s] [%(funcName)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
-def log_it(level, msg):
-    with logLock:
-        caller_frame = inspect.currentframe().f_back
-        func_name = caller_frame.f_code.co_name
-        
-        if level.lower() == "d" and not Config.DEBUG:
-            return
+handler.setFormatter(formatter)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
-        levels = {
-            "i": logging.info,
-            "e": logging.error,
-            "d": logging.info, # Use info because debug flag spams so much
-        }
-        
-        log_func = levels.get(level.lower(), None)
-        
-        if log_func:
-            if level.lower() == "d":
-                log_func(f"[DEBUG] [{func_name}] {msg}")
-            else:
-                log_func(f"[{func_name}] {msg}")
+def log_it(level, msg):
+    caller_frame = inspect.currentframe().f_back
+    func_name = caller_frame.f_code.co_name
+
+    if level.lower() == "d" and not Config.DEBUG:
+        return
+    levels = {
+        "i": logging.info,
+        "e": logging.error,
+        "d": logging.info, # Use info because debug flag spams so much
+    }
+
+    log_func = levels.get(level.lower(), None)
+
+    if log_func:
+        if level.lower() == "d":
+            log_func(f"[DEBUG] [{func_name}] {msg}")
         else:
-            logging.error(f"[{func_name}] Unsupported log level: {level}. Message: {msg}")
+            log_func(f"[{func_name}] {msg}")
+    else:
+        logging.error(f"[{func_name}] Unsupported log level: {level}. Message: {msg}")
