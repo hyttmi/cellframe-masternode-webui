0de0324a18087ec5a192fdcd263e6eed2ed15252
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-01-08 13:42:45 +0200

    Switch to tuple

--- wallets.py
+++ wallets.py
@@ -4,10 +4,14 @@ import re, inspect
 
 def get_reward_wallet_tokens(wallet):
     try:
+        reward_wallet =  {}
         cmd_get_wallet_info = cli_command(f"wallet info -addr {wallet}")
         if cmd_get_wallet_info:
             tokens = re.findall(r"coins:\s+([\d.]+)[\s\S]+?ticker:\s+(\w+)", cmd_get_wallet_info)
-            return tokens
+            if tokens:
+                for token in tokens:
+                    reward_wallet[token[1]] = token[0]
+            return reward_wallet
         else:
             return None
     except Exception as e:
