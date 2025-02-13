2764852c741641f4b25ecf46cfb1fdb760dde6f5
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-02-13 18:00:08 +0200

    ...

--- heartbeat.py
+++ heartbeat.py
@@ -51,4 +51,4 @@ def run_heartbeat_check():
     heartbeat.autocollect_status()
     heartbeat.last_signed_block()
     statuses = heartbeat.get_statuses()
-    log_it("d", f"Updated Heartbeat Statuses: {statuses}")
+    log_it("d", f"Updated heartbeat statuses: {statuses}")
