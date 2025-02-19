829b5a1fddc1a944bf5c850776b343a0780a240a
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-02-19 18:02:51 +0200

    Small improvement

--- networkutils.py
+++ networkutils.py
@@ -145,8 +145,7 @@ def get_node_data(network, only_my_node=False):
                     node['sovereign_wallet_tokens'] = None
 
             if only_my_node:
-                my_node = next((node for node in nodes if node['is_my_node']), None)
-                return my_node
+                return next((node for node in nodes if node['is_my_node']), None)
 
             info = {
                 'active_nodes_count': active_nodes_count,
