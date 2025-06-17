eb58026ae04fca8e0dc77c03933e8a2c3d75549c
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-06-17 16:10:19 +0300

    Remove retries from cli, if it doesn't respond, let it be like it.

--- common.py
+++ common.py
@@ -2,28 +2,25 @@ from command_runner import command_runner
 from logger import log_it
 import os, traceback
 
-def cli_command(command, timeout=120, is_shell_command=False, retries=3, is_from_webui=False):
-    while retries > 0:
-        try:
-            if is_shell_command:
-                exit_code, output = command_runner(command, timeout=timeout, shell=True, method='poller')
-            else:
-                exit_code, output = command_runner(f"/opt/cellframe-node/bin/cellframe-node-cli {command}", timeout=timeout, method='poller')
-            if exit_code == 0:
-                log_it("d", f"{command} executed successfully, return code was {exit_code}")
-                return output if output else True
-            elif exit_code == -254:
-                log_it("e", f"{command} timed out, retrying...")
-                if is_from_webui:
-                    log_it("e", f"Command {command} timed out, please try again later.")
-                    raise TimeoutError(f"Command {command} timed out after {timeout} seconds.")
-            else:
-                log_it("e", f"{command} failed to run successfully, return code was {exit_code}")
-                return False
-        except Exception as e:
-            log_it("e", f"An error occurred while running {command}: {e}", exc=traceback.format_exc())
-        retries -= 1
-        log_it("e", f"Retrying command: {command} ({retries} attempts left)")
+def cli_command(command, timeout=120, is_shell_command=False, is_from_webui=False):
+    try:
+        if is_shell_command:
+            exit_code, output = command_runner(command, timeout=timeout, shell=True, method='poller')
+        else:
+            exit_code, output = command_runner(f"/opt/cellframe-node/bin/cellframe-node-cli {command}", timeout=timeout, method='poller')
+
+        if exit_code == 0:
+            log_it("d", f"{command} executed successfully, return code was {exit_code}")
+            return output if output else True
+        elif exit_code == -254:
+            log_it("e", f"{command} timed out.")
+            if is_from_webui:
+                raise TimeoutError(f"Command {command} timed out after {timeout} seconds.")
+        else:
+            log_it("e", f"{command} failed to run successfully, return code was {exit_code}")
+            return False
+    except Exception as e:
+        log_it("e", f"An error occurred while running {command}: {e}", exc=traceback.format_exc())
     return None
 
 def get_current_script_directory():
--- handlers.py
+++ handlers.py
@@ -251,7 +251,7 @@ def POST_request_handler(headers, payload):
                         headers={"Content-Type": "application/json"}
                     )
 
-                result = cli_command(command, is_from_webui=True, timeout=5)
+                result = cli_command(command, timeout=5, is_from_webui=True)
                 if not result:
                     log_it("e", "CLI command returned no result")
                     return CFSimpleHTTPResponse(
