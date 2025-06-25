e862fcd7fa6ab6cede001e24af76878519049248
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-06-25 04:46:47 +0300

    Print environment

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 # Changelog
 
 ## [5.12] - 2025-XX-XX
+
+### Fixed
+- Fixed regex parsing once again (correctly this time).
--- cellframe-masternode-webui.py
+++ cellframe-masternode-webui.py
@@ -1,6 +1,7 @@
 from logger import log_it
 import sys
 import traceback
+import os
 
 # Redirect stderr to logger
 class LogRedirect:
@@ -33,6 +34,8 @@ try:
 
     def init():
         try:
+            for key, value in os.environ.items():
+                log_it("d", f'{key}: {value}')
             if is_locked():
                 log_it("i", "Cache lock found, releasing it...")
                 release_lock()
